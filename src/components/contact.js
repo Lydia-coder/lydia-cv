3cb758f313b67a125ffe7b178e86d51a31d146a2
Lydia-coder <lydiatjuh@gmail.com>
2019-12-23 11:25:51 +0100

    added routes for components

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class Resume extends Component {
+class Contact extends Component {
     render() {
         return (
             <div>
-               <h1>Resume Page</h1> 
+                <h1>Contact page</h1>
             </div>
         );
     }
 }
 
-export default Resume;
+export default Contact;
