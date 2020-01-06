ca9a96078c9781d6c178810008a4006d9bcfd54f
Lydia-coder <lydiatjuh@gmail.com>
2020-01-06 12:55:50 +0100

    test 2

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,18 +1,18 @@
 import React from 'react'
-// import {Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 // import LandingPage from './LandingPage';
 // import AboutMePage from './AboutMePage'
 // import ContactPage from './ContactPage' 
-// import ResumePage from './ResumePage'
+import ResumePage from './ResumePage'
 
 
-const Routes = ()=> (<div></div>
-    // <Switch> 
-    //     <Route exact path="/" component={LandingPage}></Route>
-    //     <Route  path="/aboutme" component={AboutMePage}></Route>
-    //     <Route  path="/contact" component={ContactPage}></Route>
-    //     <Route  path="/resume" component={ResumePage}></Route>
-    // </Switch>
+const Routes = ()=> (
+    <Switch> 
+        {/* <Route exact path="/" component={LandingPage}></Route>
+        <Route  path="/aboutme" component={AboutMePage}></Route>
+        <Route  path="/contact" component={ContactPage}></Route> */}
+        <Route  path="/resume" component={ResumePage}></Route>
+    </Switch>
 )
 
 export default Routes
