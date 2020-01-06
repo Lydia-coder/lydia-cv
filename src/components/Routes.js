9fb51805dc88032b63dbf8a132e8c9424269f072
Lydia-coder <lydiatjuh@gmail.com>
2020-01-06 12:03:45 +0100

    changed camelcase for landingpage

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
-import LandingPage from './Landingpage'
+import Landingpage from './Landingpage'
 import Aboutme from './Aboutme'
 import Contact from './Contact' 
 import Resume from './Resume'
@@ -8,7 +8,7 @@ import Resume from './Resume'
 
 const Routes = ()=> (
     <Switch> 
-        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={Landingpage}></Route>
         <Route  path="/aboutme" component={Aboutme}></Route>
         <Route  path="/contact" component={Contact}></Route>
         <Route  path="/resume" component={Resume}></Route>
