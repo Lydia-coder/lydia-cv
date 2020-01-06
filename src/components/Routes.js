a9ac0e7fa5b87874168c4951a6980be606073dcc
Lydia-coder <lydiatjuh@gmail.com>
2020-01-06 13:29:47 +0100

    changed landing page to homepage

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,12 +3,12 @@ import {Switch, Route} from 'react-router-dom'
 import AboutMePage from './AboutMePage'
 import ContactPage from './ContactPage' 
 import ResumePage from './ResumePage'
-import LandingPage from './LandingPage'
+import HomePage from './HomePage'
 
 
 const Routes = ()=> (
     <Switch> 
-        <Route  exact path="/" component={LandingPage}></Route>
+        <Route  exact path="/" component={HomePage}></Route>
         <Route  path="/aboutme" component={AboutMePage}></Route>
         <Route  path="/contact" component={ContactPage}></Route> 
         <Route  path="/resume" component={ResumePage}></Route>
